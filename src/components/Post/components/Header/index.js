118ec4c1dd44579e0f23bbcf11dc8e977418f655
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-13 16:47:50 +0530

    react-native-vector-icons library used for icons.

--- src/components/Post/components/Header/index.js
+++ src/components/Post/components/Header/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import ProfilePicture from '../../../ProfilePicture';
 import styles from './styles';
+import Icon from 'react-native-vector-icons/Entypo';
 
 const Header = ({ imageUri, name }) => {
     return (
         <View style={styles.container}>
             <ProfilePicture uri={imageUri} size={40} />
             <Text style={styles.name}>{name}</Text>
+            <Icon name="dots-three-vertical" size={20} />
         </View>
     )
 };
