5c24dfbe84be66d16fa3302f278e93169b4136b0
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-14 17:21:21 +0530

    Install tab navigator.

--- App.js
+++ App.js
@@ -6,15 +6,18 @@ import {
 
 import HomeScreen from './src/screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <HomeScreen />
-      </SafeAreaView>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={HomeScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
