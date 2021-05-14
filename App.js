2d1e8f6c6aa3a8114057769fa79f5c9ce246f239
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-14 17:10:40 +0530

    Add react native navigation.

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
 } from 'react-native';
 
 import HomeScreen from './src/screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <HomeScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <HomeScreen />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
