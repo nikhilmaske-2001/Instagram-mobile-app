847f5c017422e2b53a0f9d9eb3ceb3766a12840e
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-14 18:02:19 +0530

    Add icons for bottom navigator.

--- App.js
+++ App.js
@@ -11,13 +11,43 @@ import ProfileScreen from './src/screens/ProfileScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Foundation from 'react-native-vector-icons/Foundation';
+import Feather from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name === 'Home') {
+              return <Foundation name="home" size={size} color={color} />;
+            }
+            if (route.name === 'Discovery') {
+              return <Feather name="search" size={size} color={color} />;
+            }
+            if (route.name === 'Post') {
+              return <Feather name="plus-square" size={size} color={color} />;
+            }
+            if (route.name === 'Navigation') {
+              return <AntDesign name="hearto" size={size} color={color} />;
+            }
+            if (route.name === 'Profile') {
+              return <Ionicons name="person-outline" size={size} color={color} />;
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'black',
+          inactiveTintColor: 'gray',
+          showLabel: false
+        }}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Discovery" component={DiscoveryScreen} />
         <Tab.Screen name="Post" component={PostScreen} />
